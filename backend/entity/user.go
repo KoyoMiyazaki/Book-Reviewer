6b3cbdc8ba04573abea9d31335bc58bce0da8cf1
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-18 12:16:48 +0900

    Update: Userモデルを更新 カラム追加、および変更 構造体タグを変更

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,10 +1,10 @@
 package entity
 
 type User struct {
-	ID        uint   `gorm:"primaryKey" json:"id"`
-	FirstName string `gorm:"type:varchar(255);not null" json:"firstname" validate:"required"`
-	LastName  string `gorm:"type:varchar(255);not null" json:"lastname" validate:"required"`
-	Email     string `gorm:"type:varchar(255);unique;not null" json:"email" validate:"required,email"`
-	CreatedAt int64  `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt int64  `gorm:"autoUpdateTime" json:"updatedAt"`
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `gorm:"type:varchar(255);not null"`
+	Email     string `gorm:"type:varchar(255);unique;not null"`
+	Password  string `gorm:"type:varchar;not null"`
+	CreatedAt int64  `gorm:"autoCreateTime"`
+	UpdatedAt int64  `gorm:"autoUpdateTime"`
 }
