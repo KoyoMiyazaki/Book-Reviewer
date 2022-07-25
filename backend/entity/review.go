a48cf44db59e25322e11c4400a3e85d63a923c11
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-26 07:12:10 +0900

    Add: book、reviewエンティティを定義

---
+++ backend/entity/review.go
@@ -0,0 +1,31 @@
+package entity
+
+// レビューモデルエンティティ
+type Review struct {
+	ID        uint   `gorm:"primaryKey"`
+	Comment   string `gorm:"type:text"`
+	Rating    uint8
+	CreatedAt int64 `gorm:"autoCreateTime"`
+	UpdatedAt int64 `gorm:"autoUpdateTime"`
+	UserID    uint
+	User      User `gorm:"constraint:OnDelete:CASCADE"`
+	BookID    uint
+	Book      Book `gorm:"constraint:OnDelete:CASCADE"`
+}
+
+// レビュー登録リクエスト用構造体
+type CreateReviewRequest struct {
+	Comment           string `json:"comment" validate:"required"`
+	Rating            uint8  `json:"rating" validate:"required"`
+	UserEmail         string `json:"userEmail" validate:"required"`
+	BookTitle         string `json:"bookTitle" validate:"required"`
+	BookAuthor        string `json:"bookAuthor" validate:"required"`
+	BookThumbnailLink string `json:"bookThumbnailLink"`
+	BookPublishedDate string `json:"bookPublishedDate"`
+}
+
+// レスポンス用レビュー構造体
+type ResponseReview struct {
+	Comment string `json:"comment"`
+	Rating  uint8  `json:"rating"`
+}
