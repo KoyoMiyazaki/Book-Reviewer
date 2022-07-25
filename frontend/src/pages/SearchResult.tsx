135b672fdae39244ae09166d89191a88d7ca7865
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-26 07:29:16 +0900

    Update: 検索結果からレビューを登録できるよう更新

--- frontend/src/pages/SearchResult.tsx
+++ frontend/src/pages/SearchResult.tsx
@@ -1,15 +1,65 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import BookCard from "../components/BookCard";
-import { Grid, Stack } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  Grid,
+  Rating,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Title from "../components/Title";
+import { useAppSelector } from "../util/hooks";
 
 const SearchResult = () => {
-  const [books, setBooks] = useState<any[]>([]);
-  const { search } = useLocation();
-  const searchWord = search.split("=")[1];
   const location = useLocation();
+  const searchWord = location.search.split("=")[1];
+  const navigate = useNavigate();
+  const [books, setBooks] = useState<any[]>([]);
+  const [selectedBook, setSelectedBook] = useState({
+    title: "",
+    author: "",
+    publishedDate: "",
+    thumbnailLink: "",
+  });
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [ratingValue, setRatingValue] = useState(3);
+  const [reviewComment, setReviewComment] = useState("");
+  const user = useAppSelector((state) => state.auth.user);
+
+  const handleClickOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setRatingValue(3);
+    setReviewComment("");
+    setDialogOpen(false);
+  };
+
+  const postReviewComment = async () => {
+    // post to backend
+    const postData = {
+      comment: reviewComment,
+      rating: ratingValue,
+      userEmail: user?.email,
+      bookTitle: selectedBook.title,
+      bookAuthor: selectedBook.author,
+      bookThumbnailLink: selectedBook.thumbnailLink,
+      bookPublishedDate: selectedBook.publishedDate,
+    };
+    console.log(postData);
+    try {
+      const res = await axios.post("http://localhost:8080/review/", postData);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getBooksData = async () => {
     //   https://developers.google.com/books/docs/v1/reference/volumes/list
@@ -38,16 +88,90 @@ const SearchResult = () => {
             <Grid item xs={6}>
               <BookCard
                 title={bookInfo.title}
+                author={bookInfo.authors.join(", ")}
+                publishedDate={bookInfo.publishedDate}
                 thumbnailLink={
                   bookInfo.imageLinks ? bookInfo.imageLinks.thumbnail : ""
                 }
-                author={bookInfo.authors.join(", ")}
-                publishedDate={bookInfo.publishedDate}
+                setSelectedBook={setSelectedBook}
+                handleClickOpen={handleClickOpen}
               />
             </Grid>
           );
         })}
       </Grid>
+      {/* ダイアログ */}
+      <Dialog
+        open={dialogOpen}
+        onClose={handleClose}
+        sx={{
+          "& .MuiPaper-root": {
+            padding: "0.5rem 1rem",
+          },
+        }}
+      >
+        <Stack direction="column" spacing={2}>
+          <Typography variant="body1" component="p" fontWeight={600}>
+            {selectedBook.title}
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <img
+              src={selectedBook.thumbnailLink}
+              alt={selectedBook.title}
+              height={150}
+            />
+            <Stack direction="column">
+              <Typography variant="body2" color="text.secondary">
+                著者: {selectedBook.author}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                出版日: {selectedBook.publishedDate}
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="column">
+            <Typography variant="body2" color="text.secondary">
+              Rating
+            </Typography>
+            <Rating
+              name="half-rating"
+              value={ratingValue}
+              precision={0.5}
+              onChange={(event, newValue) => {
+                setRatingValue(newValue!);
+              }}
+            />
+          </Stack>
+          <TextField
+            label="Review Comment"
+            multiline
+            rows={4}
+            value={reviewComment}
+            onChange={(event) => {
+              setReviewComment(event.target.value);
+            }}
+          />
+        </Stack>
+
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={handleClose}
+            sx={{ textTransform: "none" }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={postReviewComment}
+            sx={{ textTransform: "none" }}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Stack>
   );
 };
