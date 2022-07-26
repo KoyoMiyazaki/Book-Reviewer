20d8485775be6737526ac2026be0eeef9d24a047
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-26 19:23:38 +0900

    Update: Bookモデルのtitle, authorの組をユニークインデックスとして設定

--- backend/entity/book.go
+++ backend/entity/book.go
@@ -3,8 +3,8 @@ package entity
 // Bookモデルエンティティ
 type Book struct {
 	ID            uint   `gorm:"primaryKey"`
-	Title         string `gorm:"type:varchar;not null;unique"`
-	Author        string `gorm:"type:varchar;not null;unique"`
+	Title         string `gorm:"type:varchar;not null;uniqueIndex:title_and_author_unique_idx"`
+	Author        string `gorm:"type:varchar;not null;uniqueIndex:title_and_author_unique_idx"`
 	ThumbnailLink string `gorm:"type:varchar"`
 	PublishedDate string `gorm:"type:varchar"`
 	CreatedAt     int64  `gorm:"autoCreateTime"`
