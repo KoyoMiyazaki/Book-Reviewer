61ffac48b8147e6f10751fd4c8a5d3550e1cf9f1
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-27 22:21:43 +0900

    Update: ユーザのログイン可否、およびレビューの有無により異なる表示をするよう更新

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,6 +13,7 @@ import axios from "axios";
 import Title from "../components/Title";
 import ReviewCard from "../components/ReviewCard";
 import { Review } from "../util/types";
+import { useAppSelector } from "../util/hooks";
 
 const Home = () => {
   const [reviews, setReviews] = useState<Review[]>([]);
@@ -26,6 +27,7 @@ const Home = () => {
     bookPublishedDate: "",
   });
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const user = useAppSelector((state) => state.auth.user);
 
   const getReviews = async () => {
     const token: string | null = localStorage.getItem("jwtToken");
@@ -97,110 +99,118 @@ const Home = () => {
 
   return (
     <Stack direction="column" maxWidth="1000px" margin="0 auto">
-      <Title title="Your Review" />
-      <Grid container spacing={2} marginTop="1rem">
-        {reviews.map((review) => {
-          return (
-            <Grid item xs={6} key={review.id}>
-              <ReviewCard
-                id={review.id}
-                comment={review.comment}
-                rating={review.rating}
-                bookTitle={review.bookTitle}
-                bookAuthor={review.bookAuthor}
-                bookThumbnailLink={review.bookThumbnailLink}
-                bookPublishedDate={review.bookPublishedDate}
-                setSelectedReview={setSelectedReview}
-                handleClickOpen={handleClickOpen}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
+      {!user ? (
+        <Title title="Please Login" />
+      ) : reviews.length === 0 ? (
+        <Title title="Let's search books you want to review!" />
+      ) : (
+        <>
+          <Title title="Your Review" />
+          <Grid container spacing={2} marginTop="1rem">
+            {reviews.map((review) => {
+              return (
+                <Grid item xs={6} key={review.id}>
+                  <ReviewCard
+                    id={review.id}
+                    comment={review.comment}
+                    rating={review.rating}
+                    bookTitle={review.bookTitle}
+                    bookAuthor={review.bookAuthor}
+                    bookThumbnailLink={review.bookThumbnailLink}
+                    bookPublishedDate={review.bookPublishedDate}
+                    setSelectedReview={setSelectedReview}
+                    handleClickOpen={handleClickOpen}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
 
-      {/* ダイアログ */}
-      <Dialog
-        open={dialogOpen}
-        onClose={handleClose}
-        sx={{
-          "& .MuiPaper-root": {
-            padding: "0.5rem 1rem",
-          },
-        }}
-      >
-        <Stack direction="column" spacing={2}>
-          <Typography variant="body1" component="p" fontWeight={600}>
-            {selectedReview.bookTitle}
-          </Typography>
-          <Stack direction="row" spacing={2}>
-            <img
-              src={selectedReview.bookThumbnailLink}
-              alt={selectedReview.bookTitle}
-              height={150}
-            />
-            <Stack direction="column">
-              <Typography variant="body2" color="text.secondary">
-                著者: {selectedReview.bookAuthor}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                出版日: {selectedReview.bookPublishedDate}
-              </Typography>
-            </Stack>
-          </Stack>
-          <Stack direction="column">
-            <Typography variant="body2" color="text.secondary">
-              Rating
-            </Typography>
-            <Rating
-              name="rating"
-              value={selectedReview.rating}
-              precision={0.5}
-              onChange={(event, newValue) => {
-                setSelectedReview((prev) => {
-                  return {
-                    ...prev,
-                    rating: newValue ? newValue : 3,
-                  };
-                });
-              }}
-            />
-          </Stack>
-          <TextField
-            label="Review Comment"
-            multiline
-            rows={4}
-            name="comment"
-            value={selectedReview.comment}
-            onChange={(event) => {
-              setSelectedReview((prev) => {
-                return {
-                  ...prev,
-                  comment: event.target.value,
-                };
-              });
+          {/* ダイアログ */}
+          <Dialog
+            open={dialogOpen}
+            onClose={handleClose}
+            sx={{
+              "& .MuiPaper-root": {
+                padding: "0.5rem 1rem",
+              },
             }}
-          />
-        </Stack>
+          >
+            <Stack direction="column" spacing={2}>
+              <Typography variant="body1" component="p" fontWeight={600}>
+                {selectedReview.bookTitle}
+              </Typography>
+              <Stack direction="row" spacing={2}>
+                <img
+                  src={selectedReview.bookThumbnailLink}
+                  alt={selectedReview.bookTitle}
+                  height={150}
+                />
+                <Stack direction="column">
+                  <Typography variant="body2" color="text.secondary">
+                    著者: {selectedReview.bookAuthor}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    出版日: {selectedReview.bookPublishedDate}
+                  </Typography>
+                </Stack>
+              </Stack>
+              <Stack direction="column">
+                <Typography variant="body2" color="text.secondary">
+                  Rating
+                </Typography>
+                <Rating
+                  name="rating"
+                  value={selectedReview.rating}
+                  precision={0.5}
+                  onChange={(event, newValue) => {
+                    setSelectedReview((prev) => {
+                      return {
+                        ...prev,
+                        rating: newValue ? newValue : 3,
+                      };
+                    });
+                  }}
+                />
+              </Stack>
+              <TextField
+                label="Review Comment"
+                multiline
+                rows={4}
+                name="comment"
+                value={selectedReview.comment}
+                onChange={(event) => {
+                  setSelectedReview((prev) => {
+                    return {
+                      ...prev,
+                      comment: event.target.value,
+                    };
+                  });
+                }}
+              />
+            </Stack>
 
-        <DialogActions>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={deleteReview}
-            sx={{ textTransform: "none" }}
-          >
-            Delete
-          </Button>
-          <Button
-            variant="contained"
-            color="success"
-            onClick={updateReview}
-            sx={{ textTransform: "none" }}
-          >
-            Update
-          </Button>
-        </DialogActions>
-      </Dialog>
+            <DialogActions>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={deleteReview}
+                sx={{ textTransform: "none" }}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={updateReview}
+                sx={{ textTransform: "none" }}
+              >
+                Update
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      )}
     </Stack>
   );
 };
