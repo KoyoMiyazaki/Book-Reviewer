e873e2f91e98772bb90e79bfbd5b9f2776aa3018
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-27 11:51:15 +0900

    Update: レビュー更新リクエスト用構造体追加、およびRatingフィールドの型変更

--- backend/entity/review.go
+++ backend/entity/review.go
@@ -4,7 +4,7 @@ package entity
 type Review struct {
 	ID        uint   `gorm:"primaryKey"`
 	Comment   string `gorm:"type:text"`
-	Rating    uint8
+	Rating    float64
 	CreatedAt int64 `gorm:"autoCreateTime"`
 	UpdatedAt int64 `gorm:"autoUpdateTime"`
 	UserID    uint
@@ -15,21 +15,27 @@ type Review struct {
 
 // レビュー登録リクエスト用構造体
 type CreateReviewRequest struct {
-	Comment           string `json:"comment" validate:"required"`
-	Rating            uint8  `json:"rating" validate:"required"`
-	BookTitle         string `json:"bookTitle" validate:"required"`
-	BookAuthor        string `json:"bookAuthor" validate:"required"`
-	BookThumbnailLink string `json:"bookThumbnailLink"`
-	BookPublishedDate string `json:"bookPublishedDate"`
+	Comment           string  `json:"comment" validate:"required"`
+	Rating            float64 `json:"rating" validate:"required,gte=0.5,lte=5.0"`
+	BookTitle         string  `json:"bookTitle" validate:"required"`
+	BookAuthor        string  `json:"bookAuthor" validate:"required"`
+	BookThumbnailLink string  `json:"bookThumbnailLink"`
+	BookPublishedDate string  `json:"bookPublishedDate"`
+}
+
+// レビュー更新リクエスト用構造体
+type UpdateReviewRequest struct {
+	Comment string  `json:"comment" validate:"required"`
+	Rating  float64 `json:"rating" validate:"required,gte=0.5,lte=5.0"`
 }
 
 // レスポンス用レビュー構造体
 type ResponseReview struct {
-	ID                uint   `json:"id"`
-	Comment           string `json:"comment"`
-	Rating            uint8  `json:"rating"`
-	BookTitle         string `json:"bookTitle"`
-	BookAuthor        string `json:"bookAuthor"`
-	BookThumbnailLink string `json:"bookThumbnailLink"`
-	BookPublishedDate string `json:"bookPublishedDate"`
+	ID                uint    `json:"id"`
+	Comment           string  `json:"comment"`
+	Rating            float64 `json:"rating"`
+	BookTitle         string  `json:"bookTitle"`
+	BookAuthor        string  `json:"bookAuthor"`
+	BookThumbnailLink string  `json:"bookThumbnailLink"`
+	BookPublishedDate string  `json:"bookPublishedDate"`
 }
