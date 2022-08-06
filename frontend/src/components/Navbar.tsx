38a0c8cff81e336d700d9ced12b50b4823444ed2
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-08-06 14:03:15 +0900

    Update: ログアウトの際、トップページに移るよう更新

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -182,6 +182,9 @@ const Navbar = () => {
                     onClick={() => {
                       handleMenuClose();
                       dispatch(logout());
+                      navigate("/", {
+                        replace: true,
+                      });
                     }}
                   >
                     {"ログアウト"}
